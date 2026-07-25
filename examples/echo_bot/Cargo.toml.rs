fdd776fa81662fe1ca30aade016edec95c557d20
allykotetsu <arisudotexe@proton.me>
2026-07-24 18:38:25 -0700

    Add example echo bot.

---
+++ examples/echo_bot/Cargo.toml.rs
@@ -0,0 +1,8 @@
+[package]
+name = "echo_bot"
+version = "0.1.0"
+edition = "2024"
+
+[dependencies]
+extism-pdk = "1.4.1"
+owncast_plugin_sdk_rust = { path = "../owncast_plugin_sdk_rust", version = "0.1.0" }
---
+++ examples/echo_bot/src/main.rs
@@ -0,0 +1,12 @@
+use owncast_plugin_sdk_rust::prelude::*;
+use owncast_plugin_sdk_rust::json_objects::chat_message::ChatMessage;
+use owncast_plugin_sdk_rust::imports::owncast_send_chat_;
+
+define_plugin!(|mut plugin_builder| {
+    plugin_builder.on_chat_message(|ChatMessage { body, .. }| {
+        owncast_send_chat_(&format!("echo {body}"));
+    });
+    Ok(plugin_builder)
+});
+
+fn main() {}
