fc1808ddb3e1d01496fc4deca867744da1599770
allykotetsu <arisudotexe@proton.me>
2026-08-04 15:56:53 -0700

    Fixed "clone could not be found" error.

--- src/plugin/define_plugin.rs
+++ src/plugin/define_plugin.rs
@@ -35,15 +35,15 @@ macro_rules! define_plugin {
         #[extism_pdk::plugin_fn]
         pub fn register() -> extism_pdk::FnResult<Manifest> {
             unsafe {
-                PLUGIN.as_ref().map_err(clone)?.dispatch_init(&mut *PLUGIN_STATE);
+                PLUGIN.as_ref().map_err(clone_error)?.dispatch_init(&mut *PLUGIN_STATE);
             }
-            Ok(PLUGIN.as_ref().map_err(clone)?.get_manifest())
+            Ok(PLUGIN.as_ref().map_err(clone_error)?.get_manifest())
         }
 
         #[extism_pdk::plugin_fn]
         pub fn on_event(envelope: Envelope) -> extism_pdk::FnResult<()> {
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_event(&mut *PLUGIN_STATE, envelope))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_event(&mut *PLUGIN_STATE, envelope))
             }
         }
 
@@ -53,28 +53,28 @@ macro_rules! define_plugin {
                 return Err(BadEventType(EventType::ChatMessageReceived, envelope.into()).into())
             };
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_filter(&mut *PLUGIN_STATE, payload))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_filter(&mut *PLUGIN_STATE, payload))
             }
         }
 
         #[extism_pdk::plugin_fn]
         pub fn on_http_request(incoming_http_request: IncomingHttpRequest) -> extism_pdk::FnResult<OutgoingHttpResponse> {
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_http_request(&mut *PLUGIN_STATE, incoming_http_request))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_http_request(&mut *PLUGIN_STATE, incoming_http_request))
             }
         }
 
         #[extism_pdk::plugin_fn]
         pub fn on_tab_content(content_request: ContentRequest) -> extism_pdk::FnResult<String> {
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_tab_content(&mut *PLUGIN_STATE, content_request).unwrap_or(String::new()))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_tab_content(&mut *PLUGIN_STATE, content_request).unwrap_or(String::new()))
             }
         }
 
         #[extism_pdk::plugin_fn]
         pub fn on_page_content(content_request: ContentRequest) -> extism_pdk::FnResult<String> {
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_page_content(&mut *PLUGIN_STATE, content_request).unwrap_or(String::new()))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_page_content(&mut *PLUGIN_STATE, content_request).unwrap_or(String::new()))
             }
         }
 
@@ -82,21 +82,21 @@ macro_rules! define_plugin {
         #[extism_pdk::plugin_fn]
         pub fn on_page_styles() -> extism_pdk::FnResult<String> {
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_page_styles(&mut *PLUGIN_STATE).unwrap_or(String::new()))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_page_styles(&mut *PLUGIN_STATE).unwrap_or(String::new()))
             }
         }
 
         #[extism_pdk::plugin_fn]
         pub fn on_page_scripts() -> extism_pdk::FnResult<String> {
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_page_scripts(&mut *PLUGIN_STATE).unwrap_or(String::new()))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_page_scripts(&mut *PLUGIN_STATE).unwrap_or(String::new()))
             }
         }
 
         #[extism_pdk::plugin_fn]
         pub fn on_auth_check(auth_check_request: AuthCheckRequest) -> extism_pdk::FnResult<AuthCheckResult> {
             unsafe {
-                Ok(PLUGIN.as_ref().map_err(clone)?.dispatch_auth_check(&mut *PLUGIN_STATE, auth_check_request).unwrap_or(AuthCheckResult::Ok))
+                Ok(PLUGIN.as_ref().map_err(clone_error)?.dispatch_auth_check(&mut *PLUGIN_STATE, auth_check_request).unwrap_or(AuthCheckResult::Ok))
             }
         }
     };
