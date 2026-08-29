b0944772d1a9dac1fa442e7252c9fd83fa5852b6
allykotetsu <arisudotexe@proton.me>
2026-08-29 15:34:13 -0700

    Change &Vec<SqlValue> params in sql functions to impl Into<Vec<SqlValue>>

--- src/owncast/sql.rs
+++ src/owncast/sql.rs
@@ -6,15 +6,15 @@ use crate::json_objects::sql_request::SqlRequest;
 use crate::json_objects::sql_row::SqlRow;
 use crate::json_objects::sql_value::SqlValue;
 
-pub fn exec(sql: &str, params: &Vec<SqlValue>) -> SharedFnResult<PartialSqlExecResult> {
-    let req = &SqlRequest::from((sql, params.clone(), None));
+pub fn exec(sql: &str, params: impl Into<Vec<SqlValue>>) -> SharedFnResult<PartialSqlExecResult> {
+    let req = &SqlRequest::from((sql, params.into(), None));
     let res = unsafe {
         owncast_sql_exec(req)
     };
     res?.try_into()
 }
 
-fn do_query(sql: &str, params: &Vec<SqlValue>, max_rows: Option<i64>) -> SharedFnResult<PartialSqlQueryResult> {
+fn do_query(sql: &str, params: Vec<SqlValue>, max_rows: Option<i64>) -> SharedFnResult<PartialSqlQueryResult> {
     let req = &SqlRequest::from((sql, params.clone(), max_rows));
     let res = unsafe {
         owncast_sql_query(req)
@@ -22,11 +22,11 @@ fn do_query(sql: &str, params: &Vec<SqlValue>, max_rows: Option<i64>) -> SharedF
     res?.try_into()
 }
 
-pub fn query(sql: &str, params: &Vec<SqlValue>) -> SharedFnResult<Vec<SqlRow>> {
-    do_query(sql, params, None)?.try_into()
+pub fn query(sql: &str, params: impl Into<Vec<SqlValue>>) -> SharedFnResult<Vec<SqlRow>> {
+    do_query(sql, params.into(), None)?.try_into()
 }
 
-pub fn query_row(sql: &str, params: &Vec<SqlValue>) -> SharedFnResult<Option<SqlRow>> {
-    let rows: Vec<SqlRow> = do_query(sql, params, Some(1))?.try_into()?;
+pub fn query_row(sql: &str, params: impl Into<Vec<SqlValue>>) -> SharedFnResult<Option<SqlRow>> {
+    let rows: Vec<SqlRow> = do_query(sql, params.into(), Some(1))?.try_into()?;
     Ok(rows.get(0).cloned())
 }
